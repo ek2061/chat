fce451355f3c9463e2bd8504552568a2729db4f2
ek2061 <jackey860517@gmail.com>
2023-05-19 11:15:54 +0800

    feat(Register): add avatar preview

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,15 +1,19 @@
 import { AuthContext } from "@/context/AuthContext";
 import { auth, db, storage } from "@/firebase";
 import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { XCircleIcon } from "@heroicons/react/24/solid";
 import { FirebaseError } from "firebase/app";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const Register: React.FC = () => {
   const { currentUser } = useContext(AuthContext);
+
+  const [img, setImg] = useState<File | null>(null); // selected image file
+  const [imgB, setImgB] = useState<string | null>(null); // selected image base64
   const [error, setError] = useState<string>("");
   const navigate = useNavigate();
 
@@ -28,16 +32,13 @@ const Register: React.FC = () => {
     const email = (form.elements.namedItem("email") as HTMLInputElement).value;
     const password = (form.elements.namedItem("password") as HTMLInputElement)
       .value;
-    const file =
-      (form.elements.namedItem("file") as HTMLInputElement).files?.[0] || null;
 
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
       const storageRef = ref(storage, `avatar/${res.user.uid}`);
 
-      const uploadTask = uploadBytesResumable(storageRef, file as Blob);
-
+      const uploadTask = uploadBytesResumable(storageRef, img as Blob);
       uploadTask.on(
         "state_changed",
         null,
@@ -69,6 +70,20 @@ const Register: React.FC = () => {
     }
   };
 
+  useEffect(() => {
+    if (img) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        setImgB(reader.result as string);
+      };
+
+      reader.readAsDataURL(img);
+    } else {
+      setImgB("");
+    }
+  }, [img]);
+
   return (
     <div className="flex h-screen items-center justify-center bg-sky-500">
       <div className="px-15 flex w-full max-w-md flex-col items-center gap-2.5 rounded-lg bg-white py-5">
@@ -98,18 +113,30 @@ const Register: React.FC = () => {
           />
           <input
             required
-            className="hidden"
             type="file"
-            name="file"
-            id="file"
+            accept="image/jpeg, image/png, image/gif"
+            className="hidden"
+            id="avatar"
+            onChange={(e) => setImg(e.target.files?.[0] || null)}
           />
           <label
             className="flex cursor-pointer items-center gap-2.5 text-sm text-cyan-800"
-            htmlFor="file"
+            htmlFor="avatar"
           >
             <UserCircleIcon className="h-6 w-6" />
             <span>Add an avatar</span>
           </label>
+
+          {imgB && (
+            <div className="relative h-20 w-20">
+              <XCircleIcon
+                className="absolute -right-2 -top-2 h-6 w-6 cursor-pointer"
+                onClick={() => setImg(null)}
+              />
+              <img className="h-20 w-20 rounded-lg object-cover" src={imgB} />
+            </div>
+          )}
+
           <button className="cursor-pointer rounded-md border-none bg-blue-400  p-2.5 font-bold text-white hover:bg-blue-500">
             Sign up
           </button>
