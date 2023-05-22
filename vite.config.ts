1d2e8990caf9eedd1a3e66d59b000c331d4557a0
ek2061 <jackey860517@gmail.com>
2023-05-22 11:18:40 +0800

    pref: code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      template: "treemap", // treemap | sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: "visualizer.html",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("firebase/firestore")) return "firestore";
+          if (id.includes("react")) return "react";
+        },
+      },
     },
   },
 });
