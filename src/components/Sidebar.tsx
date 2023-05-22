2a1d202c95e0392893926345b932fcefb464845b
ek2061 <jackey860517@gmail.com>
2023-05-22 16:52:56 +0800

    feat(Sidebar): modify class

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import UserChats from "./UserChats";
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="relative w-1/4 max-w-sm bg-sidebar max-md:w-1/3 max-sm:hidden">
+    <div className="relative flex h-[calc(100vh-64px)] w-1/4 max-w-sm flex-col bg-sidebar max-md:w-1/3 max-sm:hidden">
       <Search />
       <UserChats />
     </div>
